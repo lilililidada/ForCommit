909f7af143b607ccd3b8727537f1c2fefb01c177
joe <m13535494368@163.com>
2023-05-16 22:34:49 +0800

    修改颜色大小、

--- easyRL/AtariGame/breakout/wrapper.py
+++ easyRL/AtariGame/breakout/wrapper.py
@@ -16,8 +16,8 @@ class BreakOutWrapper(gym.Wrapper):
         self.max_frame_len = 4
         self.frame_stack = collections.deque(maxlen=self.max_frame_len)
         # 重定义observation大小
-        self.width = 84
-        self.height = 84
+        self.width = 105
+        self.height = 80
         self.observation_space = gym.spaces.Box(low=0, high=255, shape=(self.width, self.height, self.frame_num),
                                                 dtype=env.observation_space.dtype)
         # 重定义动作 0 为什么都不做 1 为左 2 为右
